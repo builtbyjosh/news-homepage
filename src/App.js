c3e23f16686c02bbfa19c77b5d82eefeeb6c52bf
builtbyjosh <joshuacollins912@gmail.com>
2023-07-16 10:36:10 -0500

    add bottom margin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App  px-8">
+    <div className="App mb-8 px-8">
       <Navbar />
       <div className=" flex flex-col md:flex-row pb-8 mb-2 md:mb-8 md:space-x-8 space-y-8 md:space-y-0 w-full">
         <MainArticle />
